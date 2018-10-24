1ba502a2cddd2aa6b4e9507b30042b4fa4c3dacc
Christian <noreply+christian@objectbox.io>
2018-10-24 13:54:42 +0200

    add empty example project

---
+++ Example/NotesExample-iOS/DetailViewController.swift
@@ -0,0 +1,34 @@
+//  Copyright © 2018 ObjectBox. All rights reserved.
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
