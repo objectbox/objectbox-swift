1a766bbc761990f525980dd693fbe4697e7aa4df
Christian <noreply+christian@objectbox.io>
2018-10-24 14:13:57 +0200

    generate Entity code with Sourcery

--- Example/NotesExample-iOS/Note.swift
+++ Example/NotesExample-iOS/Note.swift
@@ -2,15 +2,41 @@
 
 import ObjectBox
 
-class Author: ObjectBox.Entity {
-    var id: Id<Author> = 0
-    var name: String = ""
-    var notes: ToMany<Note, Author> = nil // Backlinks
+class Author: Entity {
+    var id: Id<Author>
+    var name: String
+    var notes: ToMany<Note, Author> // Backlinks
+
+    required init() {
+        self.id = 0
+        self.name = ""
+        self.notes = nil
+    }
 }
 
-class Note: ObjectBox.Entity {
+class Note: Entity {
     var id: Id<Note> = 0
     var title: String = ""
     var text: String = ""
-    var notes: ToOne<Author> = nil
+    var author: ToOne<Author> = nil
+
+    required init() {
+        self.id = 0
+        self.title = ""
+        self.text = ""
+        self.author = nil
+    }
+}
+
+// The following explicit type and property declarations are used by ToOne/ToMany relations
+// in this preliminary release. They will not be required in future releases.
+
+extension Author {
+    typealias EntityType = Author
+    var _id: Id<Author> { return id }
+}
+
+extension Note {
+    typealias EntityType = Note
+    var _id: Id<Note> { return id }
 }
