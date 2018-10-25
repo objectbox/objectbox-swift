10617350ccdc04436e32c12b2b19fc5b691aa2d5
Christian <noreply+christian@objectbox.io>
2018-10-25 13:04:33 +0200

    add macOS app target

---
+++ Example/NotesExample-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NotesExample-macOS
+//
+//  Created by Christian on 25.10.18.
+//  Copyright © 2018 ObjectBox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/NotesExample-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NotesExample-macOS
+//
+//  Created by Christian on 25.10.18.
+//  Copyright © 2018 ObjectBox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
