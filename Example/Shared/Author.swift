82b20109afa8bf7c92835af73afcaf14d87b0d3a
Christian <noreply+christian@objectbox.io>
2018-10-25 13:28:26 +0200

    split entities into separate files

---
+++ Example/Shared/Author.swift
@@ -0,0 +1,15 @@
+//  Copyright © 2018 ObjectBox. All rights reserved.
+
+import ObjectBox
+
+class Author: Entity {
+    var id: Id<Author>
+    var name: String
+    var notes: ToMany<Note, Author> // Backlinks
+
+    required init() {
+        self.id = 0
+        self.name = ""
+        self.notes = nil
+    }
+}
--- Example/Shared/Note.swift
+++ Example/Shared/Note.swift
@@ -2,18 +2,6 @@
 
 import ObjectBox
 
-class Author: Entity {
-    var id: Id<Author>
-    var name: String
-    var notes: ToMany<Note, Author> // Backlinks
-
-    required init() {
-        self.id = 0
-        self.name = ""
-        self.notes = nil
-    }
-}
-
 class Note: Entity {
     var id: Id<Note> = 0
     var title: String = ""
@@ -26,17 +14,4 @@ class Note: Entity {
         self.text = ""
         self.author = nil
     }
-}
-
-// The following explicit type and property declarations are used by ToOne/ToMany relations
-// in this preliminary release. They will not be required in future releases.
-
-extension Author {
-    typealias EntityType = Author
-    var _id: Id<Author> { return id }
-}
-
-extension Note {
-    typealias EntityType = Note
-    var _id: Id<Note> { return id }
 }
