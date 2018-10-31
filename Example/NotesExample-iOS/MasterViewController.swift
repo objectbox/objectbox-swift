e0d8582ec277f76d414462231101f7d68936e471
Christian <noreply+christian@objectbox.io>
2018-10-31 08:26:10 +0100

    add content selection list to beginning of app
    - extract NotesOverviewViewController as table controller of all notes
    - make MasterViewController show a static list of display options

--- Example/NotesExample-iOS/MasterViewController.swift
+++ Example/NotesExample-iOS/MasterViewController.swift
@@ -5,41 +5,12 @@ import ObjectBox
 
 class MasterViewController: UITableViewController {
 
-    @IBOutlet var notesTableView: UITableView!
-    var noteViewController: NoteViewController? = nil
-    var notes = [Note]()
-
-    var noteBox: Box<Note> = Services.instance.noteBox
-
-    private func configureContent() {
-        notes = noteBox.all()
-        refreshNotes()
-    }
-
-    private var noteTitleChangeSubscription: NotificationToken!
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createNote(_:)))
         navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
-            let controllers = split.viewControllers
-            noteViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? NoteViewController
-        }
 
-        configureContent()
-
-        noteTitleChangeSubscription = NotificationCenter.default.observe(name: .noteTitleDidChange, object: nil) { _ in
-            self.refreshNotes()
-        }
-    }
-
-    private func refreshNotes() {
-        guard let notesTableView = notesTableView else { return }
-        notesTableView.reloadData()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -48,14 +19,9 @@ class MasterViewController: UITableViewController {
     }
 
     @objc
-    func insertNewObject(_ sender: Any) {
-        let note = Note()
-        try! noteBox.put(note)
-        notes.insert(note, at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+    func createNote(_ sender: Any?) {
+        
     }
-
 }
 
 // MARK: - Segues
@@ -63,50 +29,8 @@ class MasterViewController: UITableViewController {
 extension MasterViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let note = notes[indexPath.row]
-                let controller = (segue.destination as! UINavigationController).topViewController as! NoteViewController
-                controller.note = note
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
-    }
-
-}
-
-// MARK: - Table View
-
-extension MasterViewController {
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let note = notes[indexPath.row]
-        cell.textLabel!.text = note.title
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            notes.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        if segue.identifier == "showNotes" {
+            // no op
         }
     }
 
---
+++ Example/NotesExample-iOS/NotesOverviewViewController.swift
@@ -0,0 +1,101 @@
+//  Copyright © 2018 ObjectBox. All rights reserved.
+
+import UIKit
+import ObjectBox
+
+class NotesOverviewViewontroller: UITableViewController {
+
+    @IBOutlet var notesTableView: UITableView!
+    var noteViewController: NoteViewController? = nil
+    var notes = [Note]()
+
+    var noteBox: Box<Note> = Services.instance.noteBox
+
+    private func configureContent() {
+        notes = noteBox.all()
+        refreshNotes()
+    }
+
+    private var noteTitleChangeSubscription: NotificationToken!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = editButtonItem
+
+        if let split = splitViewController {
+            let controllers = split.viewControllers
+            noteViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? NoteViewController
+        }
+
+        configureContent()
+
+        noteTitleChangeSubscription = NotificationCenter.default.observe(name: .noteTitleDidChange, object: nil) { _ in
+            self.refreshNotes()
+        }
+    }
+
+    private func refreshNotes() {
+        guard let notesTableView = notesTableView else { return }
+        notesTableView.reloadData()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Segues
+
+extension NotesOverviewViewontroller {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showNote" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let note = notes[indexPath.row]
+                let controller = (segue.destination as! UINavigationController).topViewController as! NoteViewController
+                controller.note = note
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+}
+
+// MARK: - Table View
+
+extension NotesOverviewViewontroller {
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let note = notes[indexPath.row]
+        cell.textLabel!.text = note.title
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            notes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+
+}
