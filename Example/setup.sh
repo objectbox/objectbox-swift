99db487261ad6ff539d6bdca5386a12d09741e1b
Shubham <noreply+shubham@objectbox.io>
2025-07-18 07:57:57 +0530

    Example: Migrate from CocoaPods to SPM

--- Example/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-# Typical ObjectBox project setup using CocoaPods; see https://swift.objectbox.io/install for details.
-
-pod repo update
-pod install
-Pods/ObjectBox/setup.rb
